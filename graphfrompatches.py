3d0e51b66c766aa941c6d1597c0c2f5f026b9af4
HafsaAkebli <hafsa.akebli@emines.um6p.ma>
2024-06-24 16:14:16 +0000

    sub

--- graphfrompatches.py
+++ graphfrompatches.py
@@ -5,3 +5,7 @@ import torch
 # define the device (GPU, else CPU)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
+
+
+#read the patches from the server that I created 
+patches_sub="/home/akebli/test5/patches/"
