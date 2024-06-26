72d7e734895e5c5c5c32ad70392a8504e95af7f7
HafsaAkebli <hafsa.akebli@emines.um6p.ma>
2024-06-26 12:35:14 +0000

    update

--- maskstolist.py
+++ maskstolist.py
@@ -5,7 +5,7 @@ import os
 def evaltile(im, cat):
     for j in range(0, y - tilesize, stride):
         for i in range(0, x - tilesize, stride):
-            tile = image[j:j + tilesize]
+            tile = image[j:j + tilesize, i:i + tilesize]
             percent = np.mean(tile)
             if percent > mincontent:
                 outstring=im+"\t"+cat+"\t"+str(i*10)+\
@@ -13,29 +13,28 @@ def evaltile(im, cat):
                 fh.write(outstring)
 
 # Parameters
-stride = 250//10 #Ensured proper sliding window movement.
+stride = 250 // 10  # Ensured proper sliding window movement
+mincontent = 0.95  # Minimum tissue content threshold for tiles
+
 maindir = "/mnt/dmif-nas/MITEL/challenges/AGGC22/ProMaL/resized-masks/train/"
 outfile = "/home/akebli/test5/try/tiles_list1.txt"
 
-# Create output file
+# Create output directory if it doesn't exist
 os.makedirs(os.path.dirname(outfile), exist_ok=True)
-fh = open(outfile, "w")
 
-imlist = os.listdir(maindir)
-mincontent = 0.95  # Minimum content threshold for tiles
-tilesize = 50  # Cause the mask is downscaled by 10 from the WSI
-tileradius = tilesize // 2
-subset = "Subset"
+with open(outfile, "w") as fh:
+    imlist = os.listdir(maindir)
+    tilesize = 50  # Because the mask is downscaled by 10 from the WSI
+    tileradius = tilesize // 2
+    subset = "Subset"
 
-for img in imlist:
-    if img.startswith(subset):
-        masklist = os.listdir(maindir + img)
-        for mask in masklist:
-            if mask.endswith(".tif"):
-                image = tifffile.imread(maindir + img + "/" + mask)
-                y, x = image.shape
-                im = img
-                cat = mask.split('_')[0]  # Extract the class name from the name of the mask
-                evaltile(im, cat)
-
-fh.close()
+    for img in imlist:
+        if img.startswith(subset):
+            masklist = os.listdir(os.path.join(maindir, img))
+            for mask in masklist:
+                if mask.endswith(".tif"):
+                    image = tifffile.imread(os.path.join(maindir, img, mask))
+                    y, x = image.shape
+                    im = img
+                    cat = mask.split('_')[0]  # Extract the class name from the name of the mask
+                    evaltile(im, cat)
