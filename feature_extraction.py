fd54d9f1d53e4ae625312e4c861985e80e577c24
HafsaAkebli <hafsa.akebli@emines.um6p.ma>
2024-07-01 20:30:46 +0000

    yes

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,17 +6,20 @@ from torchvision import transforms
 import histoencoder.functional as F
 from histoencoder.functional import extract_features
 
-
 # Define parameters
 tilesize = 500  # Size of the tile (500x500 pixels)
 tileradius = tilesize // 2  # Half the size of the tile (250 pixels)
 step = 250  # Step for the sliding window
 
+# Define the device to use (GPU if available, otherwise CPU)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("CUDA is available:", torch.cuda.is_available())
 
 # Load the pretrained HistoEncoder model
 def load_histoencoder_model(model_name: str):
     encoder = F.create_encoder(model_name)
     encoder.eval()  # Set the model to evaluation mode
+    encoder.to(device)  # Move the model to the selected device
     preprocess = transforms.Compose([
         transforms.Resize((224, 224)),  # Resize to the input size expected by the model
         transforms.ToTensor(),
@@ -25,7 +28,7 @@ def load_histoencoder_model(model_name: str):
     return encoder, preprocess
 
 # Define the model name
-model_name = 'prostate_medium' #the pretrained model of histoencoder there is also prostate_small
+model_name = 'prostate_medium'  # the pretrained model of histoencoder, there is also prostate_small
 patch_folder = "/home/akebli/test5/patches/"
 output_features_file = "/home/akebli/test5/features_prostate_medium.npz"
 
@@ -41,19 +44,19 @@ def extract_features_from_patches(encoder, preprocess, patch_folder: str):
         for patch_file in os.listdir(cls_folder):
             if patch_file.endswith(".jpg"):
                 patch_path = os.path.join(cls_folder, patch_file)
-                patch_image = Image.open(patch_path).convert("RGB")
+                patch_image = Image.open(patch_path)
 
                 # Apply transformations
                 patch_image = preprocess(patch_image)
 
                 # Convert to a PyTorch tensor and add batch dimension
-                patch_tensor = patch_image.unsqueeze(0)
+                patch_tensor = patch_image.unsqueeze(0).to(device)  # Move the tensor to the device
 
                 # Extract features
                 with torch.no_grad():
                     feature_vector = F.extract_features(
                         encoder, patch_tensor, num_blocks=1, avg_pool=False
-                    ).cpu().numpy().flatten()  # Get features from the model
+                    ).cpu().numpy().flatten()  # Get features from the model and move to CPU
 
                 # Append feature vector and label to lists
                 features.append(feature_vector)
