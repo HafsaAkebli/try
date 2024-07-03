0694ce29ca18b13c802433669d01eae9222e72dd
HafsaAkebli <hafsa.akebli@emines.um6p.ma>
2024-07-03 13:46:48 +0000

    yes

--- graph_creation.py
+++ graph_creation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import NearestNeighbors 
 
 # Path to the .npz file
 output_features_file = "/home/akebli/test5/features_prostate_medium.npz"
@@ -16,8 +16,8 @@ labels = data['labels']
 # Number of neighbors
 k = 5
 
-# Fit the NearestNeighbors model
-nn_model = NearestNeighbors(n_neighbors=k + 1)  # +1 because the nearest neighbor to a point is the point itself
+# Fit the NearestNeighbors model using Euclidean distance
+nn_model = NearestNeighbors(n_neighbors=k + 1, metric='euclidean')
 nn_model.fit(features)
 
 # Find the k-nearest neighbors for each point
@@ -41,4 +41,12 @@ pos = nx.spring_layout(G)  # Layout for visualization
 nx.draw_networkx_nodes(G, pos, node_size=50, node_color=labels, cmap=plt.cm.viridis)
 nx.draw_networkx_edges(G, pos, alpha=0.5)
 plt.title("KNN Graph of Patch Features")
+plt.show()
+
+
+# Save the figure
+figure_save_path = "/home/akebli/test5/try/graph1.png"
+plt.savefig(figure_save_path)
+
+# Show the figure
 plt.show()
