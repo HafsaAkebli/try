9ad0c1c2ebefacf9b6e2c568550335144c59ad7b
HafsaAkebli <hafsa.akebli@emines.um6p.ma>
2024-07-03 15:32:45 +0000

    yes

--- read_npz.py
+++ read_npz.py
@@ -9,12 +9,14 @@ data = np.load(output_features_file)
 # Access the features and labels
 features = data['features']
 labels = data['labels']
+patch_paths = data['patch_paths']
 
 # Check the shapes of the loaded arrays
 print("Features shape:", features.shape)
 print("Labels shape:", labels.shape)
+print("patch_paths" , patch_paths.shape)
 
 # Display the first few entries of the features and labels
 print("First 10 feature vectors:\n", features[10:])
 print("First 10 labels:\n", labels[10:])
-
+print("First 10 patch paths:\n", patch_paths[10:])
